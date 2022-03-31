c467e394a15e03790622ab455f461b57666c5c77
AbdulrasheedAdediran <abdrasheed1103@gmail.com>
2022-03-31 22:14:07 +0100

    main

---
+++ src/components/container/About/About.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+        qui.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, est.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        similique accusamus amet. Aliquam, voluptas consectetur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+        repellendus rem veritatis vitae odit ipsam corporis? Tempore quos soluta
+        quam?
+      </p>
+      <button>Back</button>
+    </div>
+  );
+};
+
+export default About;
