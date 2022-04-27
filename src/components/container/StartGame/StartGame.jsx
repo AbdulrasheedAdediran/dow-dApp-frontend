7901a9008b4659ef748dc4f64a12d263c4f473e1
AbdulrasheedAdediran <abdrasheed1103@gmail.com>
2022-04-27 01:08:50 +0100

    focus next input

--- src/components/container/StartGame/StartGame.jsx
+++ src/components/container/StartGame/StartGame.jsx
@@ -7,14 +7,45 @@ import Layout from "../../Layout";
 
 const StartGame = () => {
   const [playerInput, setPlayerInput] = useState([]);
-  // const setFocus = document.querySelector(".first-player-input");
-  // window.onload = setFocus.focus();
+  const setFocus = document.getElementsByClassName("player-input")[0];
+  window.onload = setFocus.focus();
   const handlePlayerInput = (e) => {
     const regX = /^[0-9]+$/;
     e.preventDefault();
     if (e.target.value === "" || regX.test(e.target.value)) {
       setPlayerInput([...playerInput, e.target.value]);
     }
+
+    /***======== */
+
+    var container = document.getElementsByClassName("input")[0];
+    container.onkeyup = function (e) {
+      var target = e.srcElement || e.target;
+      var maxLength = parseInt(target.attributes["maxlength"].value, 10);
+      var myLength = target.value.length;
+      if (myLength >= maxLength) {
+        var next = target;
+        while ((next = next.nextElementSibling)) {
+          if (next == null) break;
+          if (next.tagName.toLowerCase() === "input") {
+            next.focus();
+            break;
+          }
+        }
+      }
+      // Move to previous field if empty (user pressed backspace)
+      else if (myLength === 0) {
+        var previous = target;
+        while ((previous = previous.previousElementSibling)) {
+          if (previous == null) break;
+          if (previous.tagName.toLowerCase() === "input") {
+            previous.focus();
+            break;
+          }
+        }
+      }
+    };
+    /***======== */
   };
   const handlePlay = (e) => {};
   return (
